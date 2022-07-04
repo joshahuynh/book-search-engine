082b37d14b29dc552c5140e0c8831293ac4fabc2
joshahuynh <joshahuynh@gmail.com>
2022-07-04 05:27:58 -0500

    change server

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes');
 
+
 router.use('/api/users', (req,res)=> {
     console.log('/API/USERS ROUTE HIT')
 })
+
 
 router.use('/users', userRoutes);
 
